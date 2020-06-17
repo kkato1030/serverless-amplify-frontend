3057b2e003aeb27766327cf81dcf1edd3dff1e90
kkato <bsk.wisteria00@gmail.com>
2020-06-18 07:56:59 +0900

    modify: App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 下の行を修正 -->
+    <HelloWorld v-bind:msg="message"/>
   </div>
 </template>
-
+ 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+ 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  // ここから追加
+  data: function () {
+    return {
+      message: 'Loading...'
+    }
+  },
+  mounted: function () {
+    fetch('https://nfz4hxrfp1.execute-api.ap-northeast-1.amazonaws.com/dev/ping')
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (json) {
+      this.message = json.message
+    }.bind(this))
+    .catch(function (err) {
+      console.error(err)
+    })
   }
+  // ここまで追加
 }
 </script>
-
+ 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
